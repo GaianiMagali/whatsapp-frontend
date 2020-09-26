5c78ea963ed492fbae5184ebaec07843ef12c49b
GaianiMagali <magaliantonella.gaiani@gmail.com>
2020-09-26 00:22:58 -0300

    Second commit

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -4,6 +4,9 @@ import "./chat.css";
 
 import { Avatar, IconButton } from '@material-ui/core';
 import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from '@material-ui/icons/Mic';
+
 
 
 export const Chat = () => {
@@ -56,6 +59,22 @@ export const Chat = () => {
                         {new Date().toUTCString()}
                     </span>
                 </p>
+            </div>
+
+            <div className="chat__footer">
+                <InsertEmoticonIcon />
+                <form>
+                    <input
+                        placeholder="Type a message"
+                        type="text"
+                    />
+
+                    <button type="submit">
+                        Send a message
+                    </button>
+                </form>
+                
+                <MicIcon />
             </div>
         </div>
     )
